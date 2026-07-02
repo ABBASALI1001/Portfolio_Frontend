a9de81a5364285ae3c172914e600603b40f63c37
ABBASALI1001 <165723223+ABBASALI1001@users.noreply.github.com>
2026-07-02 15:24:59 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Typography, Button, Box } from "@mui/material";
 import { Download, GitHub, LinkedIn } from "@mui/icons-material";
-import profile from "../assets/portfile.jpg";
+import profile from ".../ProfileME.jpeg";
 import CV from "../assets/Abbas_FullStackWebDeveloper.pdf";
 
 const Hero = () => {
